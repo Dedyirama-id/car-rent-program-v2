f74a9a65d04a14461f1cb2537138605d55ff9835
Dedy Irama <dedyirama.ars@gmail.com>
2024-01-02 22:18:15 +0700

    Re-creating UI

---
+++ ansi_escape_sequences.h
@@ -0,0 +1,32 @@
+#ifndef ANSI_ESCAPE_SEQUENCES_H
+#define ANSI_ESCAPE_SEQUENCES_H
+
+// ANSI escape sequences for text color
+#define RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+#define GRAY "\033[90m"
+
+// ANSI escape sequences for background color
+#define BG_BLACK "\033[40m"
+#define BG_RED "\033[41m"
+#define BG_GREEN "\033[42m"
+#define BG_YELLOW "\033[43m"
+#define BG_BLUE "\033[44m"
+#define BG_MAGENTA "\033[45m"
+#define BG_CYAN "\033[46m"
+#define BG_WHITE "\033[47m"
+#define BG_GRAY "\033[100m"
+
+// ANSI escape sequences for text style
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+#define ITALIC "\033[3m"
+
+#endif // ANSI_ESCAPE_SEQUENCES_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <conio.h>
 #include <ctime>
+#include "ansi_escape_sequences.h"
 
 using namespace std;
 
@@ -57,7 +58,7 @@ int main()
         switch (choice)
         {
         case 0:
-            cout << "Exit" << endl;
+            cout << GRAY << ITALIC << "Exit" << RESET << endl;
             return 0;
         case 1:
             showCarList(data);
@@ -100,7 +101,7 @@ void showCarList(fstream &data)
     int size = getDataSize(data);
     CarRentData showCar;
 
-    cout << "[Car List]" << endl;
+    cout << BG_WHITE << BLACK << "=================== CAR LIST ===================" << RESET << endl;
     for (int i = 1; i <= size; i++)
     {
         showCar = readData(data, i);
@@ -110,7 +111,9 @@ void showCarList(fstream &data)
         cout << "   - Year \t: " << showCar.year << endl;
         cout << "   - Rent Fee \t: Rp" << showCar.rentFee << endl;
         cout << "   - Status \t: " << showCar.status << endl;
+        cout << "================================================" << endl;
     }
+    cout << GRAY << ITALIC << "[enter] to continue... ";
     cin.get();
 }
 
@@ -122,24 +125,25 @@ void writeData(fstream &data, int posisi, CarRentData &inputCar)
 
 void addCarList(fstream &data)
 {
-    int size = getDataSize(data);
     system("cls");
-    cout << "[Add New Car]" << endl;
+    cout << BG_WHITE << BLACK << "================= ADD CAR LIST =================" << RESET << endl;
+
+    int size = getDataSize(data);
     CarRentData newCar;
 
-    cout << "Registration Number: ";
+    cout << "Reg. Number\t: ";
     cin >> newCar.regNumber;
     toUpper(newCar.regNumber);
     cin.ignore();
-    cout << "Brand: ";
+    cout << "Brand\t\t: ";
     cin.getline(newCar.brand, MAX_STRING_LENGTH);
     capitalize(newCar.brand);
-    cout << "Model: ";
+    cout << "Model\t\t: ";
     cin.getline(newCar.model, MAX_STRING_LENGTH);
     capitalize(newCar.model);
-    cout << "year: ";
+    cout << "year\t\t: ";
     cin >> newCar.year;
-    cout << "Rent Fee: Rp";
+    cout << "Rent Fee\t: Rp";
     cin >> newCar.rentFee;
     strncpy(newCar.status, "Not Rented", MAX_STRING_LENGTH);
     newCar.customerId = 0;
@@ -148,7 +152,9 @@ void addCarList(fstream &data)
     newCar.rentDate = 0;
 
     writeData(data, size + 1, newCar);
-    cout << "Car added successfully!" << endl;
+    cout << "================================================" << endl;
+    cout << GREEN << ITALIC << "Car added successfully!" << RESET << endl;
+    cout << GRAY << ITALIC << "[enter] to continue... " << RESET;
     cin.ignore();
     cin.get();
 }
@@ -158,7 +164,7 @@ void editCarList(fstream &data)
     system("cls");
     int size = getDataSize(data);
 
-    cout << "[Edit Car]" << endl;
+    cout << BG_WHITE << BLACK << "=================== EDIT CAR ===================" << RESET << endl;
     cout << "Registration Number: ";
     char regNumber[MAX_STRING_LENGTH];
     cin >> regNumber;
@@ -168,43 +174,46 @@ void editCarList(fstream &data)
     if (pos >= 0)
     {
         CarRentData showCar = readData(data, pos);
-        if (showCar.status == "Not Rented")
+        if (strcmp(showCar.status, "Not Rented") == 0)
         {
             system("cls");
 
-            cout << "[Car Data]" << endl;
-            cout << "- Registration Number: " << showCar.regNumber << endl;
-            cout << "- Brand: " << showCar.brand << endl;
-            cout << "- Model: " << showCar.model << endl;
-            cout << "- Year: " << showCar.year << endl;
-            cout << "- Rent Fee (/day): Rp" << showCar.rentFee << endl;
-            cout << "- Status: " << showCar.status << endl;
+            cout << BG_BLUE << WHITE "=================== CAR DATA ===================" << RESET << endl;
+            cout << "- Reg. Number\t\t: " << showCar.regNumber << endl;
+            cout << "- Brand\t\t\t: " << showCar.brand << endl;
+            cout << "- Model\t\t\t: " << showCar.model << endl;
+            cout << "- Year\t\t\t: " << showCar.year << endl;
+            cout << "- Rent Fee (/day)\t: Rp" << showCar.rentFee << endl;
+            cout << "- Status\t\t: " << showCar.status << endl;
 
             cin.get();
             CarRentData newCar;
-            cout << "\n[New Car Data]" << endl;
-            cout << "(leave empty if you don't want to change)" << endl;
-            cout << "- Registration Number: ";
+            cout << BG_GREEN << WHITE << "\n=================== NEW CAR DATA ===================" << RESET << endl;
+            cout << GRAY << ITALIC << "[Leave empty] if you don't want to change!" << RESET << endl;
+            cout << "- Reg. Number\t\t: ";
             cin.getline(newCar.regNumber, MAX_STRING_LENGTH);
+            toUpper(newCar.regNumber);
             if (strlen(newCar.regNumber) == 0)
                 strcpy(newCar.regNumber, showCar.regNumber);
 
-            cout << "- Brand: ";
+            cout << "- Brand\t\t\t: ";
             cin.getline(newCar.brand, MAX_STRING_LENGTH);
+            capitalize(newCar.brand);
             if (strlen(newCar.brand) == 0)
                 strcpy(newCar.brand, showCar.brand);
 
-            cout << "- Model: ";
+            cout << "- Model\t\t\t: ";
             cin.getline(newCar.model, MAX_STRING_LENGTH);
+            capitalize(newCar.model);
             if (strlen(newCar.model) == 0)
                 strcpy(newCar.model, showCar.model);
 
             char buffer[MAX_STRING_LENGTH];
-            cout << "- Year: ";
+            cout << "- Year\t\t\t: ";
             cin.getline(buffer, MAX_STRING_LENGTH);
             newCar.year = (strlen(buffer) == 0) ? showCar.year : (int)atoi(buffer);
 
-            cout << "- Rent Fee (/day): ";
+            cout << "- Rent Fee (/day)\t: ";
             cin.getline(buffer, MAX_STRING_LENGTH);
             newCar.rentFee = (strlen(buffer) == 0) ? showCar.rentFee : (int)atoi(buffer);
 
@@ -215,18 +224,20 @@ void editCarList(fstream &data)
             newCar.rentDate = showCar.rentDate;
 
             writeData(data, pos, newCar);
-            cout << "Car " << regNumber << " successfully updated!" << endl;
+            cout << "================================================" << endl;
+            cout << GREEN << ITALIC << "Car " << regNumber << " successfully updated!" << RESET << endl;
         }
-        else
+        else if (strcmp(showCar.status, "Rented") == 0)
         {
-            cout << "Cannot change the data of active rented car!" << endl;
+            cout << RED << ITALIC << "Cannot change the data of active rented car!" << RESET << endl;
+            cin.ignore();
         }
     }
     else
     {
-        cout << "Car not found!" << endl;
+        cout << RED << ITALIC << "Car not found!" << RESET << endl;
+        cin.ignore();
     }
-    cin.ignore();
     cin.get();
 }
 
@@ -238,7 +249,8 @@ void removeCarList(fstream &data)
     if (temp.is_open())
     {
         int size = getDataSize(data);
-        cout << "[Remove Car]" << endl;
+        cout << BG_WHITE << BLACK << "================== Remove CAR ==================" << RESET << endl;
+
         char regNumber[MAX_STRING_LENGTH];
         cout << "Registration Number: ";
         cin >> regNumber;
@@ -260,13 +272,13 @@ void removeCarList(fstream &data)
             remove(CARS_DATA_FILE);
             temp.close();
             rename("temp.bin", CARS_DATA_FILE);
-            cout << "Car " << regNumber << " has been removed!" << endl;
+            cout << GREEN << ITALIC << "Car " << regNumber << " successfully removed!" << RESET << endl;
             cin.get();
             data.open(CARS_DATA_FILE, ios::out | ios::in | ios::binary);
         }
         else
         {
-            cout << "Car not found!" << endl;
+            cout << RED << ITALIC << "Car not found!" << RESET << endl;
             temp.close();
             remove("temp.bin");
             cin.get();
@@ -274,8 +286,8 @@ void removeCarList(fstream &data)
     }
     else
     {
-        cout << "Error when creating temporary file" << endl;
-        cout << "Please try again!" << endl;
+        cout << RED << ITALIC << "Error when creating temporary file" << RESET << endl;
+        cout << GRAY << ITALIC << "Please try again!" << RESET << endl;
         cin.get();
     }
 }
@@ -286,7 +298,8 @@ void rentCar(fstream &data)
     int size = getDataSize(data);
     CarRentData rentCar;
 
-    cout << "[Rent Car]" << endl;
+    cout << BG_WHITE << BLACK <<"=================== RENT CAR ===================" << RESET << endl;
+
     char regNumber[MAX_STRING_LENGTH];
     cout << "Registration Number\t: ";
     cin >> regNumber;
@@ -321,8 +334,8 @@ void rentCar(fstream &data)
                 }
                 else
                 {
-                    cout << "The amount paid is less than the total fee!" << endl;
-                    cout << "(ESC to back to main menu)" << endl;
+                    cout << RED << ITALIC << "The amount paid is less than the total fee!" << RESET << endl;
+                    cout << GRAY << ITALIC << "[ESC] to back to main menu" << RESET << endl;
                     cin.ignore();
                     if (_getch() == 27)
                         return;
@@ -345,15 +358,15 @@ void rentCar(fstream &data)
             cin.ignore();
             cin.get();
         }
-        else if (rentCar.status == "Rented")
+        else if (strcmp(rentCar.status, "Rented") == 0)
         {
-            cout << "Car " << regNumber << " is already rented!" << endl;
+            cout << RED << ITALIC << "Car " << regNumber << " is already rented!" << RESET << endl;
             cin.get();
         }
     }
     else
     {
-        cout << "Car not found!" << endl;
+        cout << RED << ITALIC << "Car not found!" << RESET << endl;
         cin.get();
         return;
     }
@@ -364,9 +377,10 @@ void returnCar(fstream &data)
     system("cls");
     int size = getDataSize(data);
 
-    cout << "[Return Car]" << endl;
+    cout << BLACK << BG_WHITE << BOLD << "================== RETURN CAR ==================" << RESET << endl;
+
     char regNumber[MAX_STRING_LENGTH];
-    cout << "Registration Number: ";
+    cout << "Registration Number\t: ";
     cin >> regNumber;
     toUpper(regNumber);
 
@@ -379,20 +393,20 @@ void returnCar(fstream &data)
         returnCar = readData(data, pos);
         if (returnCar.status == "Not Rented")
         {
-            cout << "Car " << regNumber << " is not rented!" << endl;
+            cout << RED << ITALIC << "Car " << regNumber << " is not rented!" << RESET << endl;
             cin.get();
             return;
         }
     }
     else
     {
-        cout << "Car not found!" << endl;
+        cout << RED << ITALIC << "Car not found!" << RESET << endl;
         cin.get();
         return;
     }
 
     int customerId;
-    cout << "Customer Id: ";
+    cout << "Customer Id\t\t: ";
     cin >> customerId;
 
     char confirm;
@@ -406,27 +420,28 @@ void returnCar(fstream &data)
     {
 
         int actualRentDuration = difftime(now, returnCar.rentDate) / (60 * 60 * 24);
-        int penaltyFee = (actualRentDuration - returnCar.rentDuration) * penaltyPercentage;
+        int penaltyFee = (actualRentDuration - returnCar.rentDuration) * penaltyPercentage * returnCar.rentFee;
 
         makeInvoice(data, returnCar, "return", now);
         if (penaltyFee > 0)
         {
             while (true)
             {
-                cout << "Penalty fee: Rp" << penaltyFee << endl;
+                cout << "Actual Duration\t: " << actualRentDuration << " days" << endl;
+                cout << "Penalty fee\t: Rp" << penaltyFee << endl;
                 int payment;
-                cout << "Payment\t: ";
+                cout << "Payment\t\t: Rp";
                 cin >> payment;
                 if (payment >= penaltyFee)
                 {
                     int refund = payment - penaltyFee;
-                    cout << "Refund\t: Rp" << refund << endl;
+                    cout << "Refund\t\t: Rp" << refund << endl;
                     break;
                 }
                 else
                 {
-                    cout << "The amount paid is less than the penalty fee!" << endl;
-                    cout << "(ESC to back to main menu)" << endl;
+                    cout << RED << ITALIC << "The amount paid is less than the penalty fee!" << RESET << endl;
+                    cout << GRAY << ITALIC << "[ESC] to back to main menu" << RESET << endl;
                     cin.ignore();
                     makeInvoice(data, returnCar, "return", now);
                     if (_getch() == 27)
@@ -451,8 +466,8 @@ void returnCar(fstream &data)
                 }
                 else
                 {
-                    cout << "Invalid input!" << endl;
-                    cout << "(ESC to back to main menu)" << endl;
+                    cout << RED << ITALIC << "Invalid input!" << RESET << endl;
+                    cout << GRAY << ITALIC << "(ESC to back to main menu)" << RESET << endl;
                     cin.ignore();
                     makeInvoice(data, returnCar, "return", now);
                     if (_getch() == 27)
@@ -463,6 +478,7 @@ void returnCar(fstream &data)
         strncpy(returnCar.status, "Not Rented", MAX_STRING_LENGTH);
         writeData(data, pos, returnCar);
         cout << "================== THANK YOU! ==================" << endl;
+        cout << GREEN << ITALIC << "Car " << regNumber << " has been successfully returned!" << RESET << endl;
 
         updateHistory(returnCar, sReturnDate, "return");
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -470,7 +486,7 @@ void returnCar(fstream &data)
     }
     else
     {
-        cout << "Invalid customer id!" << endl;
+        cout << RED << ITALIC << "Invalid customer id!" << RESET << endl;
         cin.get();
         return;
     }
@@ -478,14 +494,15 @@ void returnCar(fstream &data)
 
 int getChoice()
 {
-    cout << "[Car Rent Services]" << endl;
+    cout << BG_WHITE << BLACK << "============== CAR RENT SERVICES ==============" << RESET << endl;
     cout << "1. Show Car List" << endl;
     cout << "2. Add Car List" << endl;
     cout << "3. Edit car" << endl;
     cout << "4. Remove Car On List" << endl;
     cout << "5. Rent Car" << endl;
     cout << "6. Return Car" << endl;
-    cout << "Choose menu (0 to exit): ";
+    cout << "===============================================" << endl;
+    cout << "Choose menu [0 to exit]: ";
     int input;
     cin >> input;
     cin.ignore();
@@ -498,22 +515,24 @@ void checkDatabase(fstream &data)
     data.open(CARS_DATA_FILE, ios::out | ios::in | ios::binary);
     if (data.is_open())
     {
-        cout << "Database found!" << endl;
+        cout << GREEN << "Database found!" << RESET << endl;
     }
     else
     {
-        cout << "Database not found! Creating new one..." << endl;
+        cout << RED << "Database not found! Creating new one..." << RED << endl;
         data.close();
         data.open(CARS_DATA_FILE, ios::trunc | ios::out | ios::in | ios::binary);
     }
-    cout << "[enter] to continue! ";
+    cout << GRAY << ITALIC << "[enter] to continue! " << RESET;
     cin.get();
 }
 
-void updateHistory (CarRentData &car, char time[], string action) {
+void updateHistory(CarRentData &car, char time[], string action)
+{
     ofstream history;
     history.open(HISTORY_FILE, ios::out | ios::app);
-    if (!history.is_open()) {
+    if (!history.is_open())
+    {
         history.close();
         history.open(HISTORY_FILE, ios::trunc | ios::out | ios::app);
     }
@@ -569,9 +588,11 @@ void makeInvoice(fstream &data, CarRentData &car, string mode, time_t returnDate
     system("cls");
     if (mode == "rent")
     {
+        cout << BG_WHITE << BLACK;
         cout << "================================================" << endl;
         cout << "                CAR RENT INVOICE                " << endl;
         cout << "================================================" << endl;
+        cout << RESET;
         cout << "Customer\t: " << car.customerName << endl;
         cout << "Customer ID\t: " << car.customerId << endl;
         cout << "Reg. Number\t: " << car.regNumber << endl;
@@ -590,14 +611,17 @@ void makeInvoice(fstream &data, CarRentData &car, string mode, time_t returnDate
         char sReturnDate[80];
         strftime(sReturnDate, sizeof(sReturnDate), "%Y-%m-%d %H:%M:%S", localtime(&returnDate));
 
+        cout << BG_WHITE << BLACK;
         cout << "================================================" << endl;
-        cout << "                CAR RETURN INVOICE               " << endl;
+        cout << "                CAR RETURN INVOICE              " << endl;
         cout << "================================================" << endl;
+        cout << RESET;
         cout << "Customer\t: " << car.customerName << endl;
         cout << "Customer ID\t: " << car.customerId << endl;
         cout << "Reg. Number\t: " << car.regNumber << endl;
         cout << "Brand\t\t: " << car.brand << endl;
         cout << "Model\t\t: " << car.model << endl;
+        cout << "Rent Duration\t: " << car.rentDuration << " days" << endl;
         cout << "Rent Date\t: " << sRentDate << endl;
         cout << "Return Date\t: " << sReturnDate << endl;
         cout << "================================================" << endl;
